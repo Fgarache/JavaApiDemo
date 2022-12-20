a28d4669587d873ea69283a215032a1ca7c5f1e8
Fgarache <franciscogarach3@gmail.com>
2022-12-20 16:07:51 -0600

    master

--- src/main/java/com/example/JavaApiDemo/controllers/usuarioControllers.java
+++ src/main/java/com/example/JavaApiDemo/controllers/usuarioControllers.java
@@ -1,5 +1,7 @@
 package com.example.JavaApiDemo.controllers;
 
+import com.example.JavaApiDemo.models.Usuario;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +9,51 @@ import org.springframework.web.bind.annotation.RestController;
 public class usuarioControllers {
 @RequestMapping(value = "prueva")
     public String prueva(){
-        return "prueva";
+        return "esto es un string de prueva";
+    }
+    @RequestMapping(value = "usuario/{id}")
+    public Usuario getUsuario(@PathVariable Long id){
+        Usuario usuario = new Usuario();
+        usuario.setId(2323);
+        usuario.setNombre("Lucas");
+        usuario.setApellido("Espinoza");
+        usuario.setEmail("ejeemplo@gmail.com");
+        usuario.setTelefono("89224421");
+
+        return usuario;
+    }
+
+    @RequestMapping(value = "usuario1")
+    public Usuario editar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Lucas");
+        usuario.setApellido("Espinoza");
+        usuario.setEmail("ejeemplo@gmail.com");
+        usuario.setTelefono("89224421");
+
+        return usuario;
+    }
+
+    @RequestMapping(value = "usuario2")
+    public Usuario eliminar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Lucas");
+        usuario.setApellido("Espinoza");
+        usuario.setEmail("ejeemplo@gmail.com");
+        usuario.setTelefono("89224421");
+
+        return usuario;
+    }
+
+    @RequestMapping(value = "usuario3")
+    public Usuario buscar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Lucas");
+        usuario.setApellido("Espinoza");
+        usuario.setEmail("ejeemplo@gmail.com");
+        usuario.setTelefono("89224421");
+
+        return usuario;
     }
 }
+
