b0db8650498b368e1658afc61e679857072da3ce
Fgarache <franciscogarach3@gmail.com>
2022-12-21 10:45:10 -0600

    tablas creadas, falta implementar la conecciona la base de datos

--- src/main/java/com/example/JavaApiDemo/controllers/usuarioControllers.java
+++ src/main/java/com/example/JavaApiDemo/controllers/usuarioControllers.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class usuarioControllers {
 @RequestMapping(value = "prueva")
@@ -21,6 +24,40 @@ public class usuarioControllers {
         usuario.setTelefono("89224421");
 
         return usuario;
+    }
+
+    @RequestMapping(value = "usuarios")
+    public List<Usuario>  getUsuarios(){
+    List<Usuario> usuarios = new ArrayList<>();
+
+        Usuario usuario = new Usuario();
+        usuario.setId(2323);
+        usuario.setNombre("Lucas");
+        usuario.setApellido("Espinoza");
+        usuario.setEmail("ejeemplo@gmail.com");
+        usuario.setTelefono("89224421");
+
+        Usuario usuario2 = new Usuario();
+
+        usuario2.setId(1);
+        usuario2.setNombre("lUCAS");
+        usuario2.setApellido("Espinoza");
+        usuario2.setEmail("ejeemplo@gmail.com");
+        usuario2.setTelefono("89224421");
+
+        Usuario usuario3 = new Usuario();
+
+        usuario3.setId(2);
+        usuario3.setNombre("fRANK");
+        usuario3.setApellido("Espinoza");
+        usuario3.setEmail("ejeemplo@gmail.com");
+        usuario3.setTelefono("89224421");
+
+        usuarios.add(usuario);
+        usuarios.add(usuario2);
+        usuarios.add(usuario3);
+
+        return usuarios;
     }
 
     @RequestMapping(value = "usuario1")
